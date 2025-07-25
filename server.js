3b2084105a4bb36ab8bfa4e309b37271a5fe01db
Dhanush9866 <mahidhanush20@gmail.com>
2025-07-25 20:50:08 +0530

    vendor

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 // CORS configuration
 const corsOptions = {
   origin: [
+    "https://admin.vin2grow.in",
     "https://front3-f3cj.onrender.com",
     "https://vin2grow-latest-admin.onrender.com",
     "https://vin2grow-latest-frontend.onrender.com",
